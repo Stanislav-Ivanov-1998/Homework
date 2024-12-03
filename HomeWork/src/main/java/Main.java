bf1e057cc055c8d7947fb602298784117843a773
Stanislav <stanislavivanov177@gmail.com>
2024-12-03 22:21:13 +0300

    Add files via upload

---
+++ HomeWork/src/main/java/Main.java
@@ -0,0 +1,53 @@
+
+//для того чтобы вызывать и маркировать задачи из метода main()
+public class Main {
+    public static void main(String[] args) {
+
+        PracticalTask task = new PracticalTask();
+        System.out.println("\n Task 1:");
+        task.printThreeWords();
+
+        System.out.println("\n Task 2:");
+        task.checkSumSign();
+
+        System.out.println("\n Task 3:");
+        task.printColor();
+
+        System.out.println("\n Task 4:");
+        task.compareNumbers();
+
+        System.out.println("\n Task 5:");
+        System.out.println(task.chek(10,11));
+
+        System.out.println("\n Task 6:");
+        task.evaluator(0);
+
+        System.out.println("\n Task 7:");
+        System.out.println(task.cheking(0));
+
+        System.out.println("\n Task 8:");
+        task.printing("Не дооценил язык Java\n", 5);
+
+        System.out.println("\n Task 9:");
+        System.out.println(task.chek2(363));
+
+        System.out.println("\n Task 10:");
+        task.сhange();
+
+        System.out.println("\n Task 11:");
+        task.fill();
+
+        System.out.println("\n Task 12:");
+        task.multiply();
+
+        System.out.println("\n Task 13:");
+        task.diagonal();
+
+        System.out.println("\n Task 14:");
+        task.array(5,10);
+    }
+
+
+
+    }
+
